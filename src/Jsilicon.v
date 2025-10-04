8f0eef342cd59fb2c667152a2393cd9f8966965d
JUN-HYEOK SEO <starlights2283@gmail.com>
2025-10-04 06:06:50 +0000

    Add Module sum_cell

--- src/Jsilicon.v
+++ src/Jsilicon.v
@@ -1,2 +1,15 @@
 `define default_netname none
 
+// 지시자 추가 : 계층 변경 금지
+(* keep_hierarchy *)
+module sum_cell (
+    // 입력 정의 (2개 입력 -> return으로 )
+    input wire a,
+    input wire b,
+
+    output wire sum
+    );
+
+    // 결과 더해서 출력
+    assign sum = a + b;
+endmodule
