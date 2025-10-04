6d8033f3e239d8c286c98ee60d38851228451e39
JUN-HYEOK SEO <starlights2283@gmail.com>
2025-10-04 06:15:33 +0000

    Update Multiply_cell module

--- src/Jsilicon.v
+++ src/Jsilicon.v
@@ -3,7 +3,7 @@
 // 덧셈 모듈
 // 지시자 추가 : 계층 변경 금지
 (* keep_hierarchy *)
-module sum_cell (
+module Sum_cell (
     // 1비트 > 8비트로 업그레이드
     input wire [7:0] a,
     input wire [7:0] b,
@@ -15,8 +15,9 @@ module sum_cell (
     assign sum = a + b;
 endmodule
 
+// 뺄셈 연산 모듈
 (* keep_hierarchy *)
-module minus_cell (
+module Minus_cell (
     input wire [7:0] a,
     input wire [7:0] b,
 
@@ -24,4 +25,16 @@ module minus_cell (
     );
 
     assign minus = a - b
+endmodule
+
+// 곱셈 연산 모듈
+(* keep_hierarchy *)
+module Multiply_cell (
+    input wire [7:0] a,
+    input wire [7:0] b,
+
+    output wire [7:0] minus
+    );
+
+    assign minus = a * b
 endmodule
