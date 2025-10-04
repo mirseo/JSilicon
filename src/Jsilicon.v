0eeb2ce483409b3e3dcfd1fcf30e4f33772aa3e2
JUN-HYEOK SEO <starlights2283@gmail.com>
2025-10-04 06:16:22 +0000

    Update output wire code

--- src/Jsilicon.v
+++ src/Jsilicon.v
@@ -33,8 +33,8 @@ module Multiply_cell (
     input wire [7:0] a,
     input wire [7:0] b,
 
-    output wire [7:0] minus
+    output wire [7:0] multiply
     );
 
-    assign minus = a * b
+    assign multiply = a * b
 endmodule
