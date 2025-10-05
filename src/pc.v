3c822e610e5ae1ae81dd9504815a5bdd328aa8d7
JUN-HYEOK SEO <starlights2283@gmail.com>
2025-10-05 23:39:35 +0000

    [PC/pc.v] Add Rom inital command

--- src/pc.v
+++ src/pc.v
@@ -9,19 +9,36 @@ module PC (
     input wire reset,
     input wire ena,
 
-    output wire [7:0] instr
+    output wire [7:0] instr_out
 
     );
 
     reg [3:0] pc;
     // 하드코딩 롬 지정
-    wire [7:0] rom [0:15];
+    // wire 선언시 오류 발생 > reg로 수정
+    reg [7:0] rom [0:15];
+
+    // 내장 롬 명령어 지시 (프로그램)
+    // 명령구조 : [7:5] = opcode, [4:0]=operand 
+    // ex, ADD 3  = [000](opcode) + [00011](operand)
+    // todo - FSM 명령어 추가하기 (25.10.06)  
+    initial begin
+        // ADD 3
+        rom[0] = 8'b00000011;
+        // SUB 2
+        rom[1] = 8'b00100010;
+        // MUL 5
+        rom[2] = 8'b01000101;
+        // NOP
+        rom[3] = 8'b00000000;
+    end
 
     always @(posedge clk or posedge reset) begin
         if (reset) pc <= 0;
         else if (ena) pc <= pc + 1;
     end
 
-    assign instr = rom[pc];
+    // 포트명 오류 수정
+    assign instr_out = rom[pc];
 
 endmodule
