4528471c44a62d9a76e752a45d64b9dc2e9bdd40
JUN-HYEOK SEO <starlights2283@gmail.com>
2025-10-06 00:19:13 +0000

    [REG/regfile.v] Add Base Structure

--- src/regfile.v
+++ src/regfile.v
@@ -4,6 +4,24 @@
 
 (* keep_hierarchy *)
 module REG (
-    input wire clkm
+    input wire clock,
+    input wire reset,
+    input wire [2:0] opcode,
+
+    input wire ena
+    
     );
+
+    reg [7:0] R0, R1;
+
+    always @(posedge clock or posedge reset) begin
+        if (reset) begin
+            R0 <= 0; R1 <= 0;
+        end else if (ena) begin
+            case (opcode)
+                // opcode 별 분리
+            endcase
+        end
+    end
+    
 endmodule
