6fecd381d8155f18a201f983118aaeeeac4b7fe8
JUN-HYEOK SEO <starlights2283@gmail.com>
2025-10-07 02:50:02 +0000

    [TOP,REG/jsilicon.v,regfile.v] Restore R0/R1 data path and resolve UNDRIVEN error - Re-adding R0_out and R1_out ports to regfile.v

--- src/regfile.v
+++ src/regfile.v
@@ -14,11 +14,11 @@ module REG (
     // 명령어 저장
     input wire [7:0] data_in, 
     // FSM, ALU 데이터 저장
-    output reg [7:0] data_out
+    output reg [7:0] data_out,
 
     // 디버그 포트
-    // output wire [7:0] R0_out,
-    // output wire [7:0] R1_out
+    output wire [7:0] R0_out,
+    output wire [7:0] R1_out
     );
 
     reg [7:0] R0, R1;
@@ -49,7 +49,7 @@ module REG (
         end
     end
 
-    // assign R0_out = R0;
-    // assign R1_out = R1;
+    assign R0_out = R0;
+    assign R1_out = R1;
     
 endmodule
